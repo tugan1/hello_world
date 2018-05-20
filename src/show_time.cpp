81bd1e607c43109c55acac4d00586af7ab86ebbd
Tugan Eritenel <tugan1@gmail.com>
2018-05-20 15:22:29 +0300

    use printf in show_time

--- src/show_time.cpp
+++ src/show_time.cpp
@@ -1,5 +1,6 @@
 #include "..\\include\\show_time.h"
 void show_time(struct tm* ptm) {
-	std::cout << "Function time\t" << ptm->tm_year + 1900 << "-" << ptm->tm_mon+1 << "-" << ptm->tm_mday << " " << ptm->tm_hour << ":" << ptm->tm_min << ":" << ptm->tm_sec <<std::endl;
+	//std::cout << "Function time\t" << ptm->tm_year + 1900 << "-" << ptm->tm_mon+1 << "-" << ptm->tm_mday << " " << ptm->tm_hour << ":" << ptm->tm_min << ":" << ptm->tm_sec <<std::endl;
+	printf("%d-%02d-%02d %02d:%02d:%02d\n", ptm->tm_year+1900, ptm->tm_mon+1, ptm->tm_mday,ptm->tm_hour, ptm->tm_min, ptm->tm_sec);
 	//return void;
 }
