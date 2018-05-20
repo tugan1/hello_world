d616ba5a6efe1f79ea0ea253dd74a7af1838636d
Tugan Eritenel <tugan1@gmail.com>
2018-05-20 15:41:50 +0300

    tabs

--- include/timegm.h
+++ include/timegm.h
@@ -1,8 +1,8 @@
 #ifndef TIMEGM_H
-#define TIMEGM_H
-#ifdef _WIN32
-#ifndef timegm
-#define timegm _mkgmtime
-#endif
-#endif
+	#define TIMEGM_H
+	#ifdef _WIN32
+		#ifndef timegm
+			#define timegm _mkgmtime
+		#endif
+	#endif
 #endif
